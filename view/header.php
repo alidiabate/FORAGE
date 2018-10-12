5f59727e36517773b3d4070fb7baf01c1cfa59e6
DIABATE <alidekolia@gmail.com>
2018-10-12 12:52:24 +0000

    initial commit

---
+++ view/header.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<title>SENFORAGE</title>
+        
+        <meta charset="utf-8" />
+        
+        <link rel="stylesheet" href="../../assets/css/bootstrap.min.css" />
+        <link rel="stylesheet" href="../../assets/css/bootstrap-theme.min.css" />
+        <link rel="stylesheet" href="../../assets/js/jquery-ui/jquery-ui.min.css" />
+        <link rel="stylesheet" href="../../assets/css/style.css" />
+        <link href="../../plugins/datatables/dataTables.bootstrap.css" rel="stylesheet" type="text/css" />
+        
+        <script src="http://code.jquery.com/jquery-1.11.2.min.js"></script>
+         <script src="../../plugins/datatables/jquery.dataTables.js" type="text/javascript"></script>
+    <script src="../../plugins/datatables/dataTables.bootstrap.js" type="text/javascript"></script>
+  </head>
+  <body>
+  <!-- container section start -->
+ 
+  <section id="container" class="">
+          
+      <!--main content start-->
+      <section id="main-content">
+          <section class="wrapper">  
+          <div class="col-lg-12">
+
+          </div><!--/.row-->
+       
+              <!-- project team & activity end -->
+
+          </section>
+      </section>
+      <!--main content end-->
+  </section>
+  <!-- container section start -->  
+    </body>
+</html>
